3d4beafca33d2a469fdd89f611556bcd5de74e5f
soccompraloenacasa-ui <soc.compraloenacasa@gmail.com>
2026-01-24 18:17:01 +0100

    feat: export Customer and Distributor models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -7,6 +7,8 @@ from app.models.product import Product
 from app.models.technician import Technician
 from app.models.installation import Installation, InstallationStatus, PaymentStatus, PaymentMethod
 from app.models.inventory import InventoryMovement, MovementType
+from app.models.customer import Customer
+from app.models.distributor import Distributor, DistributorSale
 
 __all__ = [
     "User",
@@ -22,4 +24,7 @@ __all__ = [
     "PaymentMethod",
     "InventoryMovement",
     "MovementType",
+    "Customer",
+    "Distributor",
+    "DistributorSale",
 ]
